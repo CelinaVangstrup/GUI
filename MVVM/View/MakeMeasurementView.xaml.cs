c593f230a030a853adb3078c7503d4bda0fe0270
Celina <CelinaVangstrup@gmail.com>
2021-11-07 22:39:28 +0100

    Add project files.

---
+++ MVVM/View/MakeMeasurementView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ST3PRJ3.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for MakeMeasurementView.xaml
+    /// </summary>
+    public partial class MakeMeasurementView : UserControl
+    {
+        public MakeMeasurementView()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ST3PRJ3.Core;
+
+namespace ST3PRJ3.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand MakeMeasurementViewCommand { get; set; }
+
+        public RelayCommand PatientInformationViewCommand { get; set; }
+
+
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public MakeMeasurementViewModel MakeMeasurementVM { get; set; }
+        public PatientInformationViewModel PatientInformationVM { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            MakeMeasurementVM = new MakeMeasurementViewModel();
+            PatientInformationVM = new PatientInformationViewModel();
+            CurrentView = HomeVM;
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            MakeMeasurementViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = MakeMeasurementVM;
+
+            });
+
+
+            PatientInformationViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PatientInformationVM;
+
+            });
+        }
+    }
+}
